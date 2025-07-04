YSCEDC/YSCE

commit d0b71dfdc55cbe4fe6d5c214b2d0e3521d42ec9d

Make gears interact with carrier deck when aircraft center leaves deck

src/core/fsexistence.cpp
@@ -2184,12 +2184,20 @@ YSBOOL FsAirplane::HitGround(
     FsExplosionHolder * /*explosion*/)
 {
 	collType=0;
+
 	if(prop.IsAlive()==YSTRUE)
 	{
 		const YsVec3 *pos;
 		YSBOOL isOnRunway;
 
 		pos=&prop.GetPosition();
+		double elv;
+		field.GetFieldElevation(elv, pos->x(), pos->z());
+		if (pos->y() >= elv + YsTolerance) //Fix edge case where falling off carrier would kill player instead of dropping them
+		{
+			prop.SetState(FSFLYING, FSDIEDOF_NULL);
+		}
+
 		isOnRunway=YSFALSE;
 
 		if(YSTRUE==field.GetFieldShellCollision(UntransformedCollisionShell().Conv(),GetMatrix()))
@@ -2267,6 +2275,7 @@ YSBOOL FsAirplane::HitGround(
 
 						YSSCNAREATYPE areaType;
 						areaType=field.GetAreaType(GetPosition());
+						printf("Touchdown\n");
 
 						if(areaType!=YSSCNAREA_LAND)
 						{

src/core/fssimulation.cpp
@@ -4227,18 +4227,29 @@ void FsSimulation::SimProcessCollisionAndTerrain(const double & /*dt*/)
 			{
 				airPos=airPtr->GetPosition();
 				onCarrier=YSFALSE;
+				YsVec3 nose, left, right;
+				nose = airPtr->Prop().GetTirePosition(2) + airPos; //This assumes aircraft is level at touchdown
+				left = airPtr->Prop().GetTirePosition(0) + airPos;
+				right = airPtr->Prop().GetTirePosition(1) + airPos;
+
 				for(i=0; i<aircraftCarrierList.GetN(); i++)
 				{
 					FsAircraftCarrierProperty *prop;
 					prop=aircraftCarrierList[i]->Prop().GetAircraftCarrierProperty();
-					if(prop->IsOnDeck(airPos)==YSTRUE)
+					YsVec3 cPos = aircraftCarrierList[i]->Prop().GetPosition();
+					double cRadius = aircraftCarrierList[i]->Prop().GetOutsideRadius();
+					YsVec3 difference = airPos - cPos;
+					if (difference.GetLength() < cRadius)
 					{
-						// Even if flight state is FSGROUND and the airplane is not on
-						// the runway, possibly the airplane is on the flight deck of
-						// an aircraft carrier.
-						airPtr->Prop().SetOutOfRunway(YSFALSE);
-						onCarrier=YSTRUE;
-						break;
+						if (prop->IsOnDeck(nose) == YSTRUE || prop->IsOnDeck(left) == YSTRUE || prop->IsOnDeck(right) == YSTRUE)
+						{
+							// Even if flight state is FSGROUND and the airplane is not on
+							// the runway, possibly the airplane is on the flight deck of
+							// an aircraft carrier.
+							airPtr->Prop().SetOutOfRunway(YSFALSE);
+							onCarrier = YSTRUE;
+							break;
+						}
 					}
 				}
 

src/vehicle/fsairplaneproperty.cpp
@@ -1961,24 +1961,30 @@ void FsAirplaneProperty::CalculateRotation(const double &dt)
 
 void FsAirplaneProperty::CalculateCarrierLanding(const double &dt,const YsArray <FsGround *> &carrierList)
 {
-	if(staOnThisCarrier==NULL)
+	YsVec3 nose, left, right;
+
+	nose = staMatrix * chWheel;
+	left = staMatrix * chMainGearL;
+	right = staMatrix * chMainGearR;
+
+	if(staOnThisCarrier == NULL)
 	{
-		YsVec3 nose,left,right;
-
-		nose=staMatrix*chWheel;
-		left=staMatrix*chMainGearL;
-		right=staMatrix*chMainGearR;
-
 		int i;
 		for(i=0; i<carrierList.GetNumItem(); i++)
 		{
 			FsAircraftCarrierProperty *prop;
 			prop=carrierList[i]->Prop().GetAircraftCarrierProperty();
-			if(prop!=NULL && prop->LandedOnTheDeck(staPPos,staPosition,nose,left,right)==YSTRUE)
+			YsVec3 cPos = carrierList[i]->Prop().GetPosition();
+			double cRadius = carrierList[i]->Prop().GetOutsideRadius();
+			YsVec3 difference = staPosition - cPos;
+			if (difference.GetLength() < cRadius)
 			{
-				// staOnThisCarrier=carrierList[i];  This should be done from LoadAirplane
-				prop->LoadAirplane(belongTo);
-				break;
+				if (prop != NULL && prop->LandedOnTheDeck(staPPos, staPosition, nose, left, right) == YSTRUE)
+				{
+					// staOnThisCarrier=carrierList[i];  This should be done from LoadAirplane
+					prop->LoadAirplane(belongTo);
+					break;
+				}
 			}
 		}
 	}
@@ -1988,8 +1994,7 @@ void FsAirplaneProperty::CalculateCarrierLanding(const double &dt,const YsArray 
 		if(prop!=NULL)
 		{
 			YsVec3 deckNom;
-			if(prop->IsOnDeck(staPosition)!=YSTRUE ||
-			   prop->GetDeckHeightAndNormal(deckNom,staPosition)+GetGroundStandingHeight()+1.0<staPosition.y())  // 2003/04/08 Avoid catapult in the air
+			if((prop->IsOnDeck(nose)!=YSTRUE && prop->IsOnDeck(left) != YSTRUE && prop->IsOnDeck(right) != YSTRUE))  // 2003/04/08 Avoid catapult in the air  || prop->GetDeckHeightAndNormal(deckNom, staPosition) + GetGroundStandingHeight() + 1.0 < staPosition.y()
 			{
 				prop->UnloadAirplane(belongTo);  // <- This will call back AfterUnloadedFromCarrier, which cleans up stuffs.
 			}

src/vehicle/fsgroundproperty.cpp
@@ -391,22 +391,26 @@ YSBOOL FsAircraftCarrierProperty::LandedOnTheDeck
     (const YsVec3 &prv,const YsVec3 &now,
      const YsVec3 &gear1,const YsVec3 &gear2,const YsVec3 &gear3) const
 {
-	YsVec3 deckNom;
-	double deckHeight;
-	deckHeight=GetDeckHeightAndNormal(deckNom,now);
+	YsVec3 deckNom1;
+	YsVec3 deckNom2;
+	YsVec3 deckNom3;
+	double g1deckHeight = GetDeckHeightAndNormal(deckNom1, gear1);
+	double g2deckHeight = GetDeckHeightAndNormal(deckNom2, gear2);
+	double g3deckHeight = GetDeckHeightAndNormal(deckNom3, gear3);
 
 	// At least one gear must be touching the deck
-	if(gear1.y()<=deckHeight+YsTolerance ||
-	   gear2.y()<=deckHeight+YsTolerance ||
-	   gear3.y()<=deckHeight+YsTolerance)
+	if(gear1.y()<=g1deckHeight+YsTolerance ||
+	   gear2.y()<=g2deckHeight+YsTolerance ||
+	   gear3.y()<=g3deckHeight+YsTolerance)
 	{
 		// Did it approach from the top of the deck?
 		// (Previously higher than deck and Currently descending)
-		if(prv.y()>=deckHeight+YsTolerance && now.y()<=prv.y())
+		if(now.y()<=prv.y())
 		{
 			// Now above the deck
-			if(IsOnDeck(now)==YSTRUE)
+			if(IsOnDeck(gear1)==YSTRUE || IsOnDeck(gear2) == YSTRUE || IsOnDeck(gear3) == YSTRUE)
 			{
+				printf("\n");
 				return YSTRUE;
 			}
 		}

commit d75d96e21ea7a74dbbbc2316c4aa7cd0ab168db8

Approximate fix for bug introduced by d0b71df (Gear deck interaction)

src/core/fsexistence.cpp
@@ -2193,9 +2193,10 @@ YSBOOL FsAirplane::HitGround(
 		pos=&prop.GetPosition();
 		double elv;
 		field.GetFieldElevation(elv, pos->x(), pos->z());
-		if (pos->y() >= elv + YsTolerance) //Fix edge case where falling off carrier would kill player instead of dropping them
-		{
-			prop.SetState(FSFLYING, FSDIEDOF_NULL);
+		
+		if (pos->y() >= elv + YsTolerance + prop.GetOutsideRadius())    //Fix edge case where falling off carrier would kill player instead of dropping them
+		{																//Without HTRADIUS, landed aircraft will constantly HitGround but never enter landed state
+			prop.SetState(FSFLYING, FSDIEDOF_NULL);						//To do it properly, need to check the world position of the gears
 		}
 
 		isOnRunway=YSFALSE;